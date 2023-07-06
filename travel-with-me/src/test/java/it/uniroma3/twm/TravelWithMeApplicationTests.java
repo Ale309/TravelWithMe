09f12eddb4d2119da411ca3bf8e9916ccbc64598
Ale309 <ale.avadanei@gmail.com>
2023-07-06 18:55:33 +0200

    progetto vuoto

---
+++ travel-with-me/src/test/java/it/uniroma3/twm/TravelWithMeApplicationTests.java
@@ -0,0 +1,13 @@
+package it.uniroma3.twm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelWithMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
